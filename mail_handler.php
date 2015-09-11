739a928a388ca1ed4a249028530b320ce063b22d
Elie Ishimwe <elieish@gmail.com>
2015-09-11 16:14:13 +0200

    updates

---
+++ mail_handler.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['submit'])){
+    $to         = "sumba@africantraditionalpowers.com"; // this is your Email address
+    $from       = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name  = $_POST['last_name'];
+    $phone      = $_POST['phone'];
+    $subject    = "African Traditional Powers - New Contact";
+    $message    = "First Name :".$first_name . "Surname: " . $last_name . "Cell phone: ".$phone. " wrote the following:" . "\n\n" . $_POST['message'];
+
+
+    $headers    = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+
+    }
+?>
